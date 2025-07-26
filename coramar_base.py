105b13f7a1a5794eeb4c069e19d07d1b78a070df
pablo ojeda <pablojed@espol.edu.ec>
2025-07-26 13:18:01 -0500

    Actualización del método get_repartidores para atrapar excepciones

--- coramar_base.py
+++ coramar_base.py
@@ -30,15 +30,25 @@ def get_factura() :
 def get_repartidores():
     mydb = conectar()
     cursor = mydb.cursor()
+    cedula = input("Ingrese la cédula del cliente (10 caracteres): ")
+    nombre = input("Ingrese el primer nombre del cliente: ")
+    apellido = input("Ingrese el apellido del cliente: ")
+    telefono = input("Ingrese el teléfono del cliente: ")
+    categoria = input("Ingrese la categoría del cliente: ")
+    estado_cliente = input("Ingrese el estado del cliente (Activo/Inactivo): ")
+    direccion = input("Ingrese la dirección del cliente: ")
+    cod_zona = input("Ingrese el código de la zona: ")
+    cod_tarifa = input("Ingrese el código de tarifa: ")
 
-    # Consulta para obtener la cantidad de asignaciones (repartos) por repartidor
-    cursor.execute("""
-                   SELECT E.Cedula, E.Primer_Nombre, E.Apellido, COUNT(A.Cod_Ruta) AS Total_Repartos
-                   FROM Empleado E
-                   LEFT JOIN Asignacion A ON E.Cedula = A.Repartidor
-                   GROUP BY E.Cedula ,E.Primer_Nombre, E.Apellido
-                   ORDER BY Total_Repartos DESC
-                   """)
+    try:
+        cursor.execute("""
+            INSERT INTO Cliente (Cedula, Primer_Nombre, Apellido, Telefono, Categoria, Estado_cliente, Direccion, Cod_Zona, Cod_Tarifa)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (cedula, nombre, apellido, telefono, categoria, estado_cliente, direccion, cod_zona, cod_tarifa))
 
-    myresult = cursor.fetchall()
-    return myresult
+        mydb.commit()  # Confirmar la transacción
+        print("¡Cliente ingresado con éxito!")
+    except mysql.connector.Error as err:
+        print(f"Error al insertar cliente: {err}")
+
+    print(conectar())
